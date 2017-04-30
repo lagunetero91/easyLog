d92640962d0bb8ff615a07424703639c34f4d777
David Leon Ortega <david_lagu@hotmail.com>
2017-04-30 16:44:36 +0100

    cpp

--- easyLog/easyLog/easyLog.cpp
+++ easyLog/easyLog/easyLog.cpp
@@ -1,2 +1,16 @@
 ﻿#include "pch.h"
 #include "easyLog.h"
+
+string easyLog::getTime()
+{
+	time_t rawtime;
+	struct tm* stime;
+	time(&rawtime);
+	stime = localtime(&rawtime);
+	return asctime(stime);
+}
+
+string easyLog::getPatch()
+{
+	return string();
+}
