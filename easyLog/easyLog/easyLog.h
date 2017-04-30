4f17739f6fabde29adc55fe1f9b7d9c5bd0e0537
David Leon Ortega <david_lagu@hotmail.com>
2017-04-30 16:43:29 +0100

    Fichero.h

--- easyLog/easyLog/easyLog.h
+++ easyLog/easyLog/easyLog.h
@@ -1,1 +1,20 @@
 ﻿#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctime>
+#include <string>
+#include <hash_map>
+
+using namespace std;
+
+class easyLog
+{
+	bool addLogRow(string namelog, string newLogRow);
+	bool closeLog(string namelog);
+
+private:
+	hash_map<string, FILE*> logs;
+	string getTime();
+	string getPatch();
+	void newFileLog(string namelog);
+};
